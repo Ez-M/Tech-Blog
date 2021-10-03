095a3454c20e525703c21e5e7ff039d022b7af34
Ezequiel <martinez.ezequiel42@gmail.com>
2021-10-03 16:42:46 -0700

    added posts routes /new

--- routes/api/posts-routes.js
+++ routes/api/posts-routes.js
@@ -1,4 +1,28 @@
+const Post = require('../../models/post');
+const User = require('../../models/user');
+
 const router = require('express').Router();
 
-
+        // post for adding new produc tlisting
+        router.post('/new', async (req, res) => {
+            if (!req.session.loggedIn) {
+                res.redirect('/login');
+            } else {
+        
+                try {
+                    const dbPostData = await Post.create({
+                    post_name: req.body.post_name,
+                    description: req.body.description,
+                    });
+                    
+                    // res.status(200).json(dbProductData);
+                    res.redirect("/")
+                  }
+        
+                  catch (err) {
+                    console.log(err);
+                    res.status(500).json(err);
+                  }
+                } 
+       });     
 module.exports = router;
