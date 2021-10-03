8d0145958ac36fecc03b9ed83fd5d0151f95738d
Ezequiel <martinez.ezequiel42@gmail.com>
2021-10-03 11:56:00 -0700

    added user and post models, and modified main.handlebars

---
+++ models/post.js
@@ -0,0 +1,49 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize post model (table) by extending off Sequelize's Model class
+class Post extends Model {}
+
+// set up fields and rules for post model
+post.init(
+{
+    // define columns
+  
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  
+  post_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+    model: 'user',
+    key: 'id',
+    }
+  }
+},
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
---
+++ models/user.js
@@ -0,0 +1,45 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize User model (table) by extending off Sequelize's Model class
+class User extends Model {}
+
+// set up fields and rules for post model
+User.init(
+{
+    // define columns
+  
+    id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  
+  user_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  
+  email: {
+    type: DataTypes.STRING, 
+    allowNull: false,
+  }
+},
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
