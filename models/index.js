9911c27f7cf2b8fe2a6886051532ee6d38b7cbc5
Ezequiel <martinez.ezequiel42@gmail.com>
2021-10-08 01:46:40 -0700

    had to decapitalize require routes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./user');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const Post = require('./post');
+const Comment = require('./comment');
 
 // Each post can only belong to one user
 Post.belongsTo(User, {
